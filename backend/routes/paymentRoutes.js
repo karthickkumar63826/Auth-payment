4c8592ed2809398b1c72dbcf2676f1140ac962e5
karthickkumar63836 <karthickkumar63826@gmail.com>
2024-07-18 15:16:37 +0530

    updated

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const routes = express.Router();
 const makePayment = require("../controller/stripController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-routes.post("/create-checkout-session", makePayment);
+routes.post("/create-checkout-session", authMiddleware, makePayment);
 
 module.exports = routes;
